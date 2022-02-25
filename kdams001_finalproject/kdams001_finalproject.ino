eb75aae29fe5b0778b094bb3ece0bf67ae1a7ffa
Kninja99 <kcdamschen@yahoo.com>
2022-02-24 22:29:53 -0800

    btn and joystick task
    * joy stick sw not working for some reason
    
    * implemented a select btn
    
    *implemented a joystick task for left and right movement

--- kdams001_finalproject/kdams001_finalproject.ino
+++ kdams001_finalproject/kdams001_finalproject.ino
@@ -3,9 +3,16 @@
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
+
+#define joyX A1
+#define joyY A0
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-const int a0 = 6, a1 = 7, a2 = 8, a3 = 9 , a4= 10;
+const int sw = 6;
+const int a0 = 13;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// joystick var
+int xValue;
+int yValue;
 
 
 
@@ -17,46 +24,135 @@ typedef struct task {
     
 } task;
 
-const unsigned short tasksNum = 1;
+const unsigned short tasksNum = 2;
 task tasks[tasksNum];
 
-enum test_states {testStart};
-int TickFct_test(int state)
+// ------------------- Joy Stick Task --------------------------
+enum JoyStick_states {joyStart, checkDirection, left, right};
+int TickFct_JoyStick(int state)
 { 
   //Read thing
     switch(state){ // State transitions
-      case testStart:
+      case joyStart:
+        xValue = analogRead(joyX);
+        //yValue = analogRead(joyY);
+        xValue= map(xValue, 0, 1023, 0 , 3);
+        state = checkDirection;
+        break;
+      case checkDirection:
+        if(xValue > 1)
+        {
+          state =left;
+        }
+        else if(xValue < 1)
+        {
+          state = right;
+        }
+        else {
+          state = checkDirection;
+        }
+        break;
+      case left:
+        Serial.println("joystick is moved left");
+        state = checkDirection;
+        break;
+      case right:
+        Serial.println("joystick is moved right");
+        state = checkDirection;
         break;
       default:
-        state = testStart;
+        state = joyStart;
         break;
     }
     switch(state){ // State Action
-      case testStart:
+      case joyStart:
+        break;
+      case checkDirection:
+        xValue = analogRead(joyX);
+        //yValue = analogRead(joyY);
+        xValue= map(xValue, 0, 1023, 0 , 3);
+        //Serial.println(xValue);
+        break;
+      case left:
+        break;
+      case right:
         break;
       default:
         break;
     }
   return state;
   }
-
+  
+// ------------ Btn Task ------------------
+  enum Btn_states {btnStart, released, pressed};
+  
+  int TickFct_btn(int state)
+  {
+    // transitions
+    switch(state)
+    {
+      case btnStart:
+        state = released;
+        break;
+      case released:
+        if(digitalRead(a0))
+        {
+          state = pressed;
+        }
+        else if(!digitalRead(a0)){
+          state = released;
+        }
+        break;
+      case pressed:
+        if(digitalRead(a0))
+        {
+          state = pressed;
+        }
+        else if(!digitalRead(a0)){
+          Serial.println("btn was pressed");
+          state = released;
+        }
+        break;
+      default:
+        state = btnStart;
+    }
+    // ACTIONS
+    switch(state)
+    {
+      case btnStart:
+        break;
+      case released:
+        break;
+      case pressed:
+        break;
+      default:
+        break;
+    }
+    return state;
+  }
 
  
 
 void setup() {
   // Task scheduler
   unsigned char i = 0;
-  tasks[i].state = testStart;
+  tasks[i].state = joyStart;
   tasks[i].period = 100;
   tasks[i].elapsedTime = 0;
-  tasks[i].TickFct = &TickFct_test;
+  tasks[i].TickFct = &TickFct_JoyStick;
+  i++;
+  tasks[i].state = btnStart;
+  tasks[i].period = 100;
+  tasks[i].elapsedTime = 0;
+  tasks[i].TickFct = &TickFct_btn;
+  
   // pin setup
-  //pinMode(a0, INPUT);
- 
+  pinMode(13, INPUT);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
   lcd.blink();
+  lcd.write("testing");
   Serial.begin(9600);
 }
 
